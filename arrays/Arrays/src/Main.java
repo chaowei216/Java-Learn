210caecc7083f8d1f0d1fae044784a89a83fea47
chaoweiz <luutrieuvi2003@gmail.com>
2025-02-02 23:27:17 +0700

    feat: array p1

---
+++ arrays/Arrays/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Array");
+
+        // init an array
+        int[] integerArray = new int[10];
+
+        // assign value
+        integerArray[5] = 50;
+
+        // anonymous array
+        int[] firstThree = {1, 2, 3};
+
+        // compile error
+//        int[] newArray;
+//        newArray = {1, 2, 3};
+    }
+}
